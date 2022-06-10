8bb7916ff546f09150f887faea694e52ba1a334e
liyin <591459256@qq.com>
2022-06-10 09:52:36 +0800

    feat: Test checkbox component

--- examples/App.vue
+++ examples/App.vue
@@ -1,12 +1,31 @@
 <template>
   <div id="app">
-    <loading :radius="40" :border-width="4" border-color="blue" />
+    <!-- <loading :radius="40" :border-width="4" border-color="blue" /> -->
+    <AbCheckbox v-model="checked1" @change="handleChange1">同意</AbCheckbox>
+    <p>checked1: {{ checked1 }}</p>
+    <hr />
+    <DefaultCheckbox v-model="checked2" label="哈哈" @change="handleChange2"></DefaultCheckbox>
+    <p>checked2: {{ checked2 }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      checked1: true,
+      checked2: false,
+    };
+  },
+  methods: {
+    handleChange1(val) {
+      console.log('data1: ', val)
+    },
+    handleChange2(val) {
+      console.log('data2: ', val)
+    }
+  }
 }
 </script>
 
--- examples/main.js
+++ examples/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 // import AboardUI from '../packages'
-import { Loading } from '../packages'
+import { Loading, AbCheckbox, DefaultCheckbox } from '../packages'
 // import { Loading } from '../lib/index.umd.min.js'
 // import '../lib/index.css'
 
 Vue.config.productionTip = false
 // Vue.use(AboardUI)
 Vue.use(Loading)
+Vue.use(AbCheckbox)
+Vue.use(DefaultCheckbox)
 
 new Vue({
   render: h => h(App),
